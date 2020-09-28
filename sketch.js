179958b91d4390f8ef24a59183e7332464da348a
Nihar1015 <69799996+Nihar1015@users.noreply.github.com>
2020-09-28 20:43:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,13 +4,14 @@ var speed,weight
 
 function setup() {
   createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
+  
   
   speed=random(55,90);
   weight=random(400,1500);
   car= createSprite(50,200,50,50);
+  car.shapeColor=color(255);
   wall = createSprite(1500,200,60,height/2);
-  wall.shapeColor = "rgb(80,80,80)";
+  wall.shapeColor = color(80,80,80);
   car.velocityX = speed;
 
 }
